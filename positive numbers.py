1a438d6e56bcc41aca8a4a85c182556135bcfab3
dhankarthik <78548607+dhankarthik@users.noreply.github.com>
2025-02-06 10:06:59 +0530

    Add files via upload

---
+++ positive numbers.py
@@ -0,0 +1,9 @@
+a =  [12, -7, 5, 64, -14]
+b =   [12, 14, -95, 3]
+
+positive_a = [num for num in a if num > 0]
+positive_b = [num for num in b if num > 0]
+
+print("Positive numbers in a:", positive_a)
+print("Positive numbers in b:", positive_b)
+
